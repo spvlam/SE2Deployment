c8910be9a9e04a48ba24e9c648a908a56dc97bbd
spvlam <spvlam28@gmail.com>
2024-06-17 17:47:39 +0700

    config domain

--- src/enum/login.js
+++ src/enum/login.js
@@ -4,9 +4,9 @@ class LoginDevices{
     static refreshToken = 1;
 } 
 class frontEndDirect{
-    static directREGISTERSUCCESS = "se-deploy-fr.vercel.app/";
-    static loginFacebookSuccess = "se-deploy-fr.vercel.app/success2authen"
-     static vnpayTransactionResult = "se-deploy-fr.vercel.app/vnpay"
+    static directREGISTERSUCCESS = "se-deploy-fr.vercel.app";
+    static loginFacebookSuccess = "https://se-deploy-fr.vercel.app/success2authen"
+     static vnpayTransactionResult = "https://se-deploy-fr.vercel.app/vnpay"
 }
 
 module.exports = {LoginDevices,frontEndDirect}
